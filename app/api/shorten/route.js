89907412e16a54172eab09eff4ee974f00c85878
Pranav2302 <21118023@pvgcoet.ac.in>
2024-12-26 16:06:02 +0530

    up112

--- app/api/shorten/route.js
+++ app/api/shorten/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   const { url } = await req.json();
-  // Implement your URL shortening logic here
+  
   const shortUrl = `https://url-shortening-backend-delta.vercel.app/${Math.random()
     .toString(36)
     .substr(2, 6)}`;
